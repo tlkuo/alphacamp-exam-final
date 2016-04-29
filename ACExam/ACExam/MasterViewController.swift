ad7700b9de105e4e6f116714f1b94aeec4b0ae2b
martin <tlkuo.frb@gmail.com>
2016-04-29 16:54:46 +0800

    Fix crash issue

--- ACExam/ACExam/MasterViewController.swift
+++ ACExam/ACExam/MasterViewController.swift
@@ -40,10 +40,10 @@ class MasterViewController: UITableViewController {
 
     func insertNewObject(sender: AnyObject) {
 //        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+//        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+//        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
-    
+
     func getData() {
 
         NetworkManager.sharedInstance.getData({ books in
